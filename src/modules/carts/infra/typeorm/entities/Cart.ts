9c1f3522e1c5f79e6202a6b556fa7b0b2ae29648
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2025-03-05 21:46:33 -0300

    Create Cart.ts

---
+++ src/modules/carts/infra/typeorm/entities/Cart.ts
@@ -0,0 +1,16 @@
+import { Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
+
+import { CartItem } from './CartItem';
+
+import { AbstractEntity } from '@/shared/infra/typeorm/entities/AbstractEntity';
+import { User } from '@/modules/users/infra/typeorm/entities/User';
+
+@Entity('carts')
+export class Cart extends AbstractEntity {
+  @OneToOne(() => User, (user) => user.cart)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => CartItem, (cartItem) => cartItem.cart)
+  cartItems: CartItem[];
+}
