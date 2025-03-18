8e9510e6f826517fa1cd8000e69526854d868481
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2025-03-17 21:59:50 -0300

    add productsRepository and IProductsRepository

---
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,37 @@
+import { AbstractTransactionRepository } from '@/shared/container/providers/transaction-manager/AbstractTransactionRepository';
+import { Product } from '../entities/Products';
+import {
+  IProductsRepository,
+  ProductSaveInput,
+  ProductUpdate,
+} from '@/modules/products/repositories/IProductsRepository';
+import { AppDataSource } from '@/shared/infra/typeorm';
+import { TransactionManager } from '@/shared/container/providers/transaction-manager/TransactionManager';
+import { injectable } from 'tsyringe';
+
+@injectable()
+export class ProductRepository
+  extends AbstractTransactionRepository<Product>
+  implements IProductsRepository
+{
+  private readonly productRepository = AppDataSource.getRepository(Product);
+  constructor(protected transaction: TransactionManager) {
+    super(transaction, Product);
+  }
+  async create(data: ProductSaveInput) {
+    const product = this.productRepository.create(data);
+    return await this.productRepository.save(product);
+  }
+  async findById(id: string) {
+    return await this.productRepository.findOne({ where: { id } });
+  }
+  async listAll() {
+    return await this.productRepository.find();
+  }
+  async delete(id: string) {
+    await this.productRepository.delete(id);
+  }
+  async update(data: ProductUpdate) {
+    await this.productRepository.update({ id: data.id }, data);
+  }
+}
---
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -0,0 +1,21 @@
+import { StrictOmit } from '@/shared/util/types/StrictOmitType';
+import { Product } from '../infra/typeorm/entities/Products';
+
+export type ProductSaveInput = StrictOmit<
+  Product,
+  'id' | 'category' | 'cartItems' | 'orderItems'
+>;
+export type ProductUpdate = StrictOmit<
+  Product,
+  'category' | 'cartItems' | 'orderItems'
+>;
+
+interface IProductsRepository {
+  create(data: ProductSaveInput): Promise<Product>;
+  findById(id: string): Promise<Product | null>;
+  listAll(): Promise<Product[]>;
+  delete(id: string): Promise<void>;
+  update(data: ProductUpdate): Promise<void>;
+}
+
+export { IProductsRepository };
