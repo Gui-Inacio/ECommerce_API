88bcf98fb896ac2dbc4f13ad5ff364809515219a
Guilherme Inácio <44714868+Gui-Inacio@users.noreply.github.com>
2025-03-25 21:05:22 -0300

    create CartItem interface repository

---
+++ src/modules/carts/repositories/ICartItemRepository.ts
@@ -0,0 +1,15 @@
+import { StrictOmit } from '@/shared/util/types/StrictOmitType';
+import { CartItem } from '../infra/typeorm/entities/CartItem';
+
+export type CartItemSaveImput = StrictOmit<CartItem, 'id' | 'product' | 'cart'>;
+export type CartItemUpdate = StrictOmit<CartItem, 'product' | 'cart'>;
+
+interface IcartItemRepository {
+  create(data: CartItemSaveImput): Promise<CartItem>;
+  findById(id: string): Promise<CartItem>;
+  listAll(): Promise<CartItem[]>;
+  delete(id: string): Promise<void>;
+  update(data: CartItemUpdate): Promise<void>;
+}
+
+export { IcartItemRepository };
